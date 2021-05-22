0e3a11a18e3d6f3645b7adcb6c5314a49d312d54
L-MCO0001404 <johnrohit94@gmail.com>
2021-05-23 04:15:15 +0530

    added

--- HostelAllocationSystem/settings.py
+++ HostelAllocationSystem/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '98_euom-gff4!05()l$v!ehnve28a#d(cftpr5%jce1l75j_qq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','hos2001.herokuapp.com']
 
 
 # Application definition
